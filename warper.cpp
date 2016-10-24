203da3648f595a0e453397adc99dba5fa9592682
zchan0 <zchan0@outlook.com>
2016-10-24 16:58:34 -0400

    Add basic affine warp

--- warper.cpp
+++ warper.cpp
@@ -10,6 +10,7 @@
 #include <cstdlib>
 #include <vector>
 #include <cmath>
+#include <limits>
 
 #include "ImageIO/ImageIO.h"
 #include "Matrix/matrix.h"
@@ -24,7 +25,15 @@ typedef enum
   Projective = 'p'
 } Transform;
 
+static const unsigned char ESC = 27;
+
+static int inW, inH, outW, outH;
+static unsigned char *outPixmap;
 static std::string input, output;
+
+static ImageIO ioOrigin = ImageIO();
+static ImageIO ioWarped = ImageIO();
+
 static Matrix3D sepcMatrix  = Matrix3D(); // Single transform matrix
 static Matrix3D multiMatrix = Matrix3D(); // Prodcut of multiple matrices
 
@@ -106,7 +115,8 @@ bool executeOperation(const char spec, double *params)
       sepcMatrix.setelement(2, 1, params[1]);
       break;
     case Projective:
-      std::cout << "Projective!" << std::endl;
+      sepcMatrix.setelement(3, 1, params[0]);
+      sepcMatrix.setelement(3, 2, params[1]);
       break;
     default:
       std::cerr << "ERROR: undefined transform specification OR invalid params" << std::endl;
@@ -114,12 +124,7 @@ bool executeOperation(const char spec, double *params)
       break;
   }
 
-  std::cout << "sepcMatrix " << std::endl;
-  sepcMatrix.print();
-
   multiMatrix = sepcMatrix * multiMatrix;
-  std::cout << "multiMatrix" << std::endl;
-  multiMatrix.print();
 
   return true; 
 }
@@ -140,12 +145,12 @@ bool parseOperation(std::string operation)
   return executeOperation(v[0][0], params);
 }
 
-void inputOperations()
+bool inputOperations()
 {
 	std::string line;
 	while(std::getline(std::cin, line)) {
 		if (line == "d") {
-			break;
+			return true;
     }
 		
     if(!parseOperation(line)) {
@@ -153,30 +158,192 @@ void inputOperations()
       continue;
     }
 	}
+  return false;
 }
 
 /** warp */
 
-void X(double u, double v)
+void forwardMap(float u, float v, float &x, float &y)
 {
-  double sum = 0.0;
-  std::cout << multiMatrix[0] << std::endl; 
+  Vector2D inVector, outVector;
+  inVector.x = u;
+  inVector.y = v;
+  outVector = multiMatrix * inVector;
+  x = outVector.x;
+  y = outVector.y;
 }
 
-void Y(double u, double v)
+void inverseMap(float x, float y, float &u, float &v)
 {
-  double sum = 0.0;
-  std::cout << multiMatrix[1] << std::endl; 
+  Vector2D inVector, outVector;
+  inVector.x = x;
+  inVector.y = y;
+  outVector = multiMatrix.inverse() * inVector;  
+  u = outVector.x;
+  v = outVector.y;
 }
 
-void inverseMap()
+/**
+ * init outpixmap with out width and out height
+ * set alpha channel to 0, makes pixel without color to be transparent
+ */
+void setupOutPixmap(int outW, int outH)
 {
+  outPixmap = new unsigned char[RGBA * outW * outH];
+  for (int i = 0; i < outH; ++i) 
+    for (int j = 0; j < outW; ++j) 
+      for (int channel = 0; channel < RGBA; ++channel)
+        outPixmap[(i * outW + j) * RGBA + channel] = 0;
+}
 
+/** 
+ * calculate output image size, with 4 corners of input image
+ */
+void setupOutSize(int &outW, int &outH)
+{
+  float minX = std::numeric_limits<float>::max();
+  float minY = minX;
+  float maxX = std::numeric_limits<float>::min();
+  float maxY = maxX;
+
+  int *us = new int[2]; // u coordinates
+  int *vs = new int[2]; // v coordinates
+  float *xs = new float[2]; // x coordinates
+  float *ys = new float[2]; // y coordinates
+
+  us[0] = 0;  us[1] = inW; 
+  vs[0] = 0;  vs[1] = inH;
+  xs[0] = 0;  xs[1] = 0;
+  ys[0] = 0;  ys[1] = 0;
+
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      forwardMap(us[i], vs[j], xs[i], ys[j]);
+      minX = fmin(minX, xs[i]);
+      minY = fmin(minY, ys[i]);
+      maxX = fmax(maxX, xs[i]);
+      maxY = fmax(maxY, ys[i]);
+    }
+  }
+
+  std::cout << "\ntranslation (" << -minX << ", " << -minY << ")" << std::endl;
+  
+  //  adjust coordinates by translation
+  sepcMatrix.setidentity();
+  if (minX < 0) {
+    sepcMatrix.setelement(1, 3, -minX);
+  } 
+  if (minY < 0) {
+    sepcMatrix.setelement(2, 3, -minY);
+  }
+
+  multiMatrix = sepcMatrix * multiMatrix;
+  std::cout << "after translation, multiMatrix " << std::endl;
+  multiMatrix.print();
+
+  // calculate output size
+  // need round up max/min value before substraction
+  maxX = std::ceil(maxX);  maxY = std::ceil(maxY);
+  minX = std::floor(minX); minY = std::floor(minY);
+  outW = maxX - minX;
+  outH = maxY - minY;
+  std::cout << "outW " << outW << "\toutH " << outH << std::endl;  
+  // init outPixmap with calculated w / h
+  setupOutPixmap(outW, outH);
+}
+
+void warp(const unsigned char *inPixmap)
+{
+  setupOutSize(outW, outH);
+
+  int k, l;
+  float u, v;
+  for (int y = 0; y < outH; ++y) {
+    for (int x = 0; x < outW; ++x) {
+      inverseMap(x, y, u, v);
+      k = (int)std::floor(v);
+      l = (int)std::floor(u);
+
+      if (k < 0 || k > inH || l < 0 || l > inW) 
+        continue;
+
+      for (int channel = 0; channel < RGBA; ++channel) 
+        outPixmap[(y * outW + x) * RGBA + channel] = inPixmap[(k * inW + l) * RGBA + channel];
+    }
+  }
+
+}
+
+void loadImage()
+{
+  ioOrigin.loadFile(input);
+  // setup input image's width and height
+  inW = ioOrigin.getWidth();
+  inH = ioOrigin.getHeight();
+
+  warp(ioOrigin.pixmap);
+  ioWarped.setPixmap(outW, outH, outPixmap);
+}
+
+void displayOriginWindow() 
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  ioOrigin.draw();
+}
+
+void displayWarpedWindow()
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  ioWarped.draw();
+}
+
+void handleReshape(int width, int height) 
+{
+  glViewport(0, 0, width, height);
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0, width, 0, height);
+  glMatrixMode(GL_MODELVIEW); 
+}
+
+void handleKeyboard(unsigned char key, int x, int y) 
+{
+  switch(key) {
+    case 'w': case 'W': 
+      ioWarped.saveImage(output); break;
+    case 'q': case 'Q': case ESC: 
+      exit(0); break;
+  } 
 }
 
 int main(int argc, char *argv[])
 {
-	inputOperations();
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	
+  if (parseCommandLine(argc, argv) && inputOperations()) {
+      loadImage();  
+  }
+
+  // Origin image window
+  glutInitWindowSize(inW, inH);
+  glutCreateWindow("Original Image");
+  glutDisplayFunc(displayOriginWindow);
+  glutKeyboardFunc(handleKeyboard);
+  glutReshapeFunc(handleReshape);
+
+  // Warped image window
+  glutInitWindowSize(outW, outH);
+  glutInitWindowPosition(glutGet(GLUT_WINDOW_X) + inW, 0);
+  glutCreateWindow("Warped Image");
+  glutDisplayFunc(displayWarpedWindow);
+  glutKeyboardFunc(handleKeyboard);
+  glutReshapeFunc(handleReshape);
+
+  glutMainLoop();
 
 	return 0;
 }
